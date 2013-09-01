0708ec4e9108e46cf65e3a8b56425175ac7f5e2b
Bruna Xavier <brunabxsantos@gmail.com>
2013-08-31 23:37:32 -0300

    Desenhando circulo
    - Deslocamento em X e em Y

--- start.m
+++ start.m
@@ -1,11 +1,15 @@
 function start()
+    % deslocamento
+    dx = 0;
+    dy = 0;
+    
     % cria a imagem 1000 x 1000
     imagem = zeros([1000 1000]);
     
     % gera imagem
     X = ones(1000, 1) * [-500 : 499];
     Y = [-500 : 499]' * ones(1, 1000); 
-    Z = X.^2 + Y.^2; 
+    Z = (X + dx).^2 + (Y + dy).^2; 
     imagem(find(Z <= 10^2)) = 1;
     
     % exibe imagem
