6ab0cb7fdec1a750f808c50df41921dd4085f9b4
Bruna Xavier <brunabxsantos@gmail.com>
2013-09-01 00:02:41 -0300

    Fixando raio em 5px

--- desenha_circulo.m
+++ desenha_circulo.m
@@ -2,6 +2,9 @@ function imagem = desenha_circulo(imagem, dx, dy, tonalidade)
     % tamanho da imagem
     [width, height] = size(imagem);
 
+    % raio fico em 5px
+    raio = 5;
+    
     % gera a matriz de tamanho width x height
     % com o circulo na posicao (dx, dy) onde o ponto (0, 0) encontra-se no centro da matriz
     X = ones(width, 1) * [-ceil(width/2) : ceil(width/2)-1];
@@ -9,5 +12,5 @@ function imagem = desenha_circulo(imagem, dx, dy, tonalidade)
     Z = (X - dx).^2 + (Y - dy).^2; 
 
     % gera imagem
-    imagem(find(Z <= 10^2)) = tonalidade; 
+    imagem(find(Z <= raio^2)) = tonalidade; 
 end
