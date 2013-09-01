3f955391e6ef91baf673238b09bfb582bd577944
Bruna Xavier <brunabxsantos@gmail.com>
2013-08-31 23:46:51 -0300

    Imagem com tamanho reduzido a 256^2

--- start.m
+++ start.m
@@ -1,6 +1,6 @@
 function start()   
-    % cria a imagem 1000 x 1000
-    imagem = zeros([1000 1000]);
+    % cria a imagem 256 x 256
+    imagem = zeros([256 256]);
     
     % gera imagem 
     imagem = desenha_circulo(imagem, 0, 0, 128)
