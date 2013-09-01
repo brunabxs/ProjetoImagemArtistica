3d2cd16a95ef6c71a2b70b9a3687bb22e23a6d28
Bruna Xavier <brunabxsantos@gmail.com>
2013-08-31 23:55:39 -0300

    Desenhando circulo (Correção)

--- desenha_circulo.m
+++ desenha_circulo.m
@@ -6,7 +6,7 @@ function imagem = desenha_circulo(imagem, dx, dy, tonalidade)
     % com o circulo na posicao (dx, dy) onde o ponto (0, 0) encontra-se no centro da matriz
     X = ones(width, 1) * [-ceil(width/2) : ceil(width/2)-1];
     Y = [-ceil(height/2) : ceil(height/2)-1]' * ones(1, height); 
-    Z = (X + dx).^2 + (Y + dy).^2; 
+    Z = (X - dx).^2 + (Y - dy).^2; 
 
     % gera imagem
     imagem(find(Z <= 10^2)) = tonalidade; 
