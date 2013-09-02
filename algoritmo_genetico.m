c4d2f212037a71ba0052fe11eab8e92e78b0a53f
Bruna Xavier <brunabxsantos@gmail.com>
2013-09-02 12:08:56 -0300

    Algoritmo genético
    - Incluindo algoritmo genetico

--- algoritmo_genetico.m
+++ algoritmo_genetico.m
@@ -1,5 +1,37 @@
-%opcoes = struct('atributos', 3, 'bits_atributo', 3, 'circulos', 2,
-%'raio_circulo', 5, 'imagem', 8)
+function [imagem, individuo_perfeito, circulos] = algoritmo_genetico()
+    %mapa de cores
+    mapa_cores = [0:1 / 255:1]' * ones(1,3);
+    mapa_cores(1, :) = [0 0 0];
+    
+    % REPENSAR ISSO AQUI!!!!
+    opcoes = struct('atributos', 3, 'bits_atributo', 8, 'circulos', 50, 'raio_circulo', 10, 'imagem', 256, 'mapa_cores', mapa_cores);
+
+    % algoritmo genetico
+    total_genes = opcoes.bits_atributo * opcoes.atributos * opcoes.circulos;
+    opcoes_genetico = gaoptimset('PopulationSize', 10, 'PopulationType', 'bitstring', 'Generations', 1, 'SelectionFcn', @selectionroulette, 'CrossoverFraction', 0.8);
+    [individuo_perfeito, avaliacao_individuo_perfeito] = ga(@funcao_avaliacao, total_genes, [], [], [], [], [], [], [], opcoes_genetico);
+    
+    % exibe imagem para individuo final
+    imagem = desenhar_individuo(individuo_perfeito, opcoes);
+    imshow(imagem, opcoes.mapa_cores);
+    
+    % individuo
+    circulos = gerar_individuo(individuo_perfeito, opcoes.bits_atributo, opcoes.atributos, opcoes.circulos);
+end
+
+function resultado = funcao_avaliacao(cromossomo)
+    % REPENSAR ISSO AQUI!!!!
+    opcoes = struct('atributos', 3, 'bits_atributo', 8, 'circulos', 50, 'raio_circulo', 10, 'imagem', 256, 'mapa_cores', []);
+    
+    % gera a imagem
+    imagem = desenhar_individuo(cromossomo, opcoes);
+    %imagem = mat2gray(imagem);
+    
+    % compara com a imagem original
+    [imagem_original, mapa_cores] = imread('imagem.bmp');
+    
+    resultado = sum(sum(imagem - double(imagem_original)));
+end
 
 function imagem = desenhar_individuo(individuo, opcoes)
     % converte a sequencia de bits para uma matriz contendo as informacoes
@@ -12,7 +44,7 @@ function imagem = desenhar_individuo(individuo, opcoes)
     % desenha o circulo correspondente na imagem
     for i = 1:opcoes.circulos;
         imagem = desenhar_circulo(imagem, individuo(i,1), individuo(i,2), opcoes.raio_circulo, individuo(i,3));
-    end        
+    end  
 end
 
 function individuo = gerar_individuo(cromossomo, genes_atributo, total_atributos, total_circulos)
