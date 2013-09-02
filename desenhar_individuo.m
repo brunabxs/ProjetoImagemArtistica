9557d3affc7ced3bbffc45275a72971eca0f73a1
Bruna Xavier <brunabxsantos@gmail.com>
2013-09-02 08:28:45 -0300

    Desenhando individuo
    - Funçao desenha_circulo passa a ser local

--- desenhar_individuo.m
+++ desenhar_individuo.m
@@ -11,8 +11,8 @@ function imagem = desenhar_individuo(individuo, opcoes)
     
     % desenha o circulo correspondente na imagem
     for i = 1:opcoes.circulos;
-        imagem = desenha_circulo(imagem, individuo(i,1), individuo(i,2), individuo(i,3));
-    end    
+        imagem = desenhar_circulo(imagem, individuo(i,1), individuo(i,2), opcoes.raio_circulo, individuo(i,3));
+    end        
 end
 
 function individuo = gerar_individuo(cromossomo, genes_atributo, total_atributos, total_circulos)
@@ -31,4 +31,18 @@ function individuo = gerar_individuo(cromossomo, genes_atributo, total_atributos
     for i = 1:total_circulos
         individuo(i,:) = bi2de(circulos(:,:,i), 'left-msb')';
     end
+end
+
+function imagem = desenhar_circulo(imagem, dx, dy, raio, tonalidade)
+    % tamanho da imagem
+    [largura, altura] = size(imagem);
+    
+    % gera a matriz de tamanho largura x altura
+    % com o circulo na posicao (dx, dy) onde o ponto (0, 0) encontra-se no centro da matriz
+    X = ones(largura, 1) * [-ceil(largura/2) : ceil(largura/2)-1];
+    Y = [-ceil(altura/2) : ceil(altura/2)-1]' * ones(1, altura); 
+    Z = (X - dx).^2 + (Y - dy).^2; 
+
+    % gera imagem
+    imagem(find(Z <= raio^2)) = tonalidade; 
 end
