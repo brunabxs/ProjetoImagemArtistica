6cd4c3553bd6a181b2b9306fd483264180c66ce5
Bruna Xavier <brunabxsantos@gmail.com>
2013-09-22 11:10:27 -0300

    Parametrização da função
    - Número de círculos parametrizado algoritmo_genetico
    - Variação do raio parametrizado algoritmo_genetico
    - Imagem parametrizada na função algoritmo_genetico
    - Criação de diretório para guardar resultados
    - Funções de avaliação com e sem comparação de resoluções
    - Removendo avisos da sintax Matlab

--- algoritmo_genetico.m
+++ algoritmo_genetico.m
@@ -1,20 +1,23 @@
-function [imagem, melhor_individuo, avaliacao, circulos, tempo] = algoritmo_genetico()
+function [imagem, melhor_individuo, avaliacao, circulos, tempo] = algoritmo_genetico(circulos, variacao_raio, nome_imagem)
     start = tic;
     matlabpool open 4;
     
     % imagem
-    [imagem_original, mapa_cores_original] = imread('imagem.bmp');
+    [imagem_original, mapa_cores_original] = imread(strcat(nome_imagem, '.bmp'));
     
     % converter para escala de cinza
     imagem_original = ind2gray(imagem_original, mapa_cores_original);
     
     % opcoes
-    opcoes = struct('bits_atributo', [8, 8, 8, 5], 'circulos', 1000, 'imagem', 256, 'diretorio_saida', 'solucao/');
+    opcoes = struct('bits_atributo', [8, 8, 8, variacao_raio], 'circulos', circulos, 'imagem', 256, 'diretorio_saida', strcat('solucao-', num2str(circulos), '-', num2str(2^variacao_raio - 1),'-', nome_imagem, '/'));
+    
+    % cria a pasta para guardar as imagens
+    mkdir(opcoes.diretorio_saida);
     
     % algoritmo genetico
     total_genes = sum(opcoes.bits_atributo) * opcoes.circulos;
-    opcoes_genetico = gaoptimset('PopulationSize', 20, 'PopulationType', 'bitstring', 'Generations', 2, 'SelectionFcn', @selectionroulette, 'CrossoverFraction', 0.8, 'UseParallel', 'always', 'Vectorized', 'off', 'OutputFcns', @(opcoes_saida, estado, flags)gerar_imagem_geracao(opcoes_saida, estado, flags, opcoes));
-    [melhor_individuo, avaliacao, flag, saida] = ga(@(cromossomo)funcao_avaliacao(cromossomo, opcoes, imagem_original), total_genes, [], [], [], [], [], [], [], opcoes_genetico);
+    opcoes_genetico = gaoptimset('PopulationSize', 20, 'PopulationType', 'bitstring', 'Generations', 3000, 'SelectionFcn', @selectionroulette, 'CrossoverFraction', 0.8, 'UseParallel', 'always', 'Vectorized', 'off', 'OutputFcns', @(opcoes_saida, estado, flags)gerar_imagem_geracao(opcoes_saida, estado, flags, opcoes));
+    [melhor_individuo, avaliacao, flag, saida] = ga(@(cromossomo)funcao_avaliacao_resolucao_sem_peso(cromossomo, opcoes, imagem_original), total_genes, [], [], [], [], [], [], [], opcoes_genetico);
     
     % exibe imagem para individuo final
     imagem = desenhar_individuo(melhor_individuo, opcoes);
@@ -34,11 +37,21 @@ function [imagem, melhor_individuo, avaliacao, circulos, tempo] = algoritmo_gene
     fprintf(arquivo, 'Avaliacao do Melhor Individuo: %f\r\n', avaliacao);
     fprintf(arquivo, 'Geracoes: %d\r\n', saida.generations);
     fprintf(arquivo, 'Termino(%d): %s\r\n\r\n', flag, saida.message);
+    fprintf(arquivo, 'Tamanho da imagem: %d\r\n', opcoes.imagem);
+    fprintf(arquivo, 'Total de circulos: %d\r\n', opcoes.circulos);
+    fprintf(arquivo, 'Variacao dos raios dos circulos: %d\r\n\r\n', 2^opcoes.bits_atributo(1, 4) - 1);
     fprintf(arquivo, 'Tempo: %f\r\n\r\n', tempo);
     fclose(arquivo);
 end
 
-function resultado = funcao_avaliacao(cromossomo, opcoes, imagem_original)
+function resultado = funcao_avaliacao_simples(cromossomo, opcoes, imagem_original)
+    % exibe a imagem para individuo
+    imagem = desenhar_individuo(cromossomo, opcoes);
+    
+    resultado = sum(sum((imagem_original - double(imagem)).^2));
+end
+
+function resultado = funcao_avaliacao_resolucao_sem_peso(cromossomo, opcoes, imagem_original)
     % exibe a imagem para individuo
     imagem = desenhar_individuo(cromossomo, opcoes);
     
@@ -46,7 +59,7 @@ function resultado = funcao_avaliacao(cromossomo, opcoes, imagem_original)
     [largura] = size(imagem, 1);
     tamanho_max = log2 (largura);
     base = 2 * ones(1, tamanho_max + 1);
-    expoente = - 1 * [0 : tamanho_max];
+    expoente = - 1 * (0 : tamanho_max);
     escalas = base.^expoente;
     
     resultado = 0;
@@ -58,6 +71,8 @@ function resultado = funcao_avaliacao(cromossomo, opcoes, imagem_original)
         % comparacao
         resultado = sum(sum((A - double(B)).^2)) + resultado;
     end
+    
+    %resultado = sum(sum((imagem_original - double(imagem)).^2));
 end
 
 function [estado, opcoes_saida, opcoes_saida_alterdas] = gerar_imagem_geracao(opcoes_saida, estado, flags, opcoes)
@@ -106,22 +121,22 @@ function individuo = gerar_individuo(cromossomo, genes_atributo, total_circulos)
     
     % posicao X
     total_genes = 0;
-    X = circulos(:, [1:genes_atributo(1)] + total_genes);
+    X = circulos(:, (1:genes_atributo(1)) + total_genes);
     X = bi2de(X, 'left-msb');
     
     % posicao Y
     total_genes = total_genes + genes_atributo(1);
-    Y = circulos(:, [1:genes_atributo(2)] + total_genes);
+    Y = circulos(:, (1:genes_atributo(2)) + total_genes);
     Y = bi2de(Y, 'left-msb');
     
     % tonalidade
     total_genes = total_genes + genes_atributo(2);
-    T = circulos(:, [1:genes_atributo(3)] + total_genes);
+    T = circulos(:, (1:genes_atributo(3)) + total_genes);
     T = bi2de(T, 'left-msb');
     
     % raio do circulo
     total_genes = total_genes + genes_atributo(3);
-    R = circulos(:, [1:genes_atributo(4)] + total_genes);
+    R = circulos(:, (1:genes_atributo(4)) + total_genes);
     R = bi2de(R, 'left-msb');
     
     % individuo
@@ -134,8 +149,8 @@ function imagem = desenhar_circulo(imagem, dx, dy, raio, tonalidade)
     
     % gera a matriz de tamanho largura x altura
     % com o circulo na posicao (dx, dy) onde o ponto (0, 0) encontra-se no centro da matriz
-    X = ones(largura, 1) * [1 : largura];
-    Y = [1 :altura]' * ones(1, altura); 
+    X = ones(largura, 1) * (1 : largura);
+    Y = (1 : altura)' * ones(1, altura); 
     Z = (X - dx).^2 + (Y - dy).^2; 
 
     % gera imagem
